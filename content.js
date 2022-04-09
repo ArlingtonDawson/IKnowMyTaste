57a96491219fe6c4b9f4daba17ad5fc87dc7e02e
Arlington Dawson <arlington.dawson@gmail.com>
2022-04-09 17:22:38 -0400

    Add project files.

---
+++ content.js
@@ -0,0 +1,19 @@
+
+function nodeInsertedCallback(event) {
+
+    if (event.srcElement.ariaLabel === "Recommended Channels") {
+        console.log("We Should Hide");
+        event.srcElement.classList.remove("dzgehN");
+        event.srcElement.className = '';
+        event.srcElement.style.display = "none";
+    }
+
+    console.log(event)
+    var divs = document.querySelectorAll('div[class$="find-me"]');
+    console.log("Recommened: " + divs.length);
+
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.display = "none";
+    }
+};
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
